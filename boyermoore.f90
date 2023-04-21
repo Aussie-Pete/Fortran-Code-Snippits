0f78f730b7747d98b4c60f2ff07c7bde7962a1b5
Aussie-Pete <47000750+Aussie-Pete@users.noreply.github.com>
2023-04-21 18:33:44 +1000

    Update boyermoore.f90

--- boyermoore.f90
+++ boyermoore.f90
@@ -9,14 +9,14 @@
 !
        contains
 !
-       pure subroutine badcharheuristic(Str,Sizex,Badchar)
+       pure subroutine badcharheuristic(Str,M,Badchar)
        implicit none
 ! Dummy arguments
 !
-       integer  ::  Sizex
+       integer  ::  M
        integer , dimension(0:NO_OF_CHARS-1)  ::  Badchar
-       character(1) , dimension(0:Sizex)  ::  Str
-       intent (in) Sizex , Str
+       character(1) , dimension(0:M-1)  ::  Str
+       intent (in) M , Str
        intent (out) Badchar
 !
 ! Local variables
@@ -25,7 +25,7 @@
 ! Code starts here
         Badchar(0:NO_OF_CHARS - 1) = -1
  
-       do i = 0 , Sizex - 1
+       do i = 0 , M - 1
            Badchar(iachar(Str(i))) = i
        enddo
        return
@@ -38,14 +38,13 @@
 !
        integer  ::  M
        integer  ::  N
-       character(len=m)   ::  Pat
-       character(len=n)   ::  Str
+       character(len=1),DIMENSION(0:M-1)   ::  Pat
+       character(len=1),DIMENSION(0:N-1)   ::  Str
        intent (in) M , N , Pat , Str
 !
 ! Local variables
 !
-       integer , dimension(N)  ::  arr
-       integer , dimension(NO_OF_CHARS)  ::  badchar
+       integer , dimension(0:NO_OF_CHARS-1)  ::  badchar
        integer  ::  found
        integer  ::  i
        integer  ::  j
@@ -59,23 +58,21 @@
        if ( (M==0) .OR. (N==0) .OR. (M>N) ) return
        badchar = 0
        call badcharheuristic(Pat,M,badchar)
-       arr = 0
        i = 0
        s = 0
        do while ( s<=(N-M) )
-           j = M
-           do while ( (j >= 1) .AND. (Pat(j:j) == Str(s+j:s+j)) )
+           j = M-1
+           do while ( (j >= 0) .AND. (Pat(j) == Str(s+j)) )
                j = j - 1
                if ( j < 1 ) then
                    found = s + 1
                    return
                endif
            enddo
-           i = badchar(iachar(Str(s+j:s+j)))
-           s = s + (j-i)
+           i = badchar(iachar(Str(s+j)))
+           s = s + MAX(1,j-i)
        enddo
        found = -1
        return
        end function bmfort
        end module bm_fort
-!
